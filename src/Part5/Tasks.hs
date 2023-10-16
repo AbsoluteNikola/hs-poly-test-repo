b3cb0c30d9f32d5e6fa1ebdf17513ee04e487c25
Nikolay Rulev <n.rulev@tinkoff.ru>
2023-10-16 19:48:04 +0300

    done part 5

--- src/Part5/Tasks.hs
+++ src/Part5/Tasks.hs
@@ -4,29 +4,34 @@ import Util(notImplementedYet)
 
 -- Реализуйте левую свёртку
 myFoldl :: (b -> a -> b) -> b -> [a] -> b
-myFoldl = notImplementedYet
+myFoldl f start [] = start
+myFoldl f start (x:xs) = myFoldl f (f start x) xs
 
 -- Реализуйте правую свёртку
 myFoldr :: (a -> b -> b) -> b -> [a] -> b
-myFoldr = notImplementedYet
+myFoldr  f start [] = start
+myFoldr f start (x:xs) = f x (myFoldr f start xs)
 
 -- Используя реализации свёрток выше, реализуйте все остальные функции в данном файле
 
 myMap :: (a -> b) -> [a] -> [b]
-myMap f = notImplementedYet
+myMap f = myFoldr (\x b -> f x : b) []
 
 myConcatMap :: (a -> [b]) -> [a] -> [b]
-myConcatMap f = notImplementedYet
+myConcatMap f = myFoldr (\x b -> f x <> b) []
 
 myConcat :: [[a]] -> [a]
-myConcat = notImplementedYet
+myConcat = myConcatMap id
 
 myReverse :: [a] -> [a]
-myReverse = notImplementedYet
+myReverse = myFoldl (flip (:)) []
 
 myFilter :: (a -> Bool) -> [a] -> [a]
-myFilter p = notImplementedYet
+myFilter p = myFoldr f []
+  where
+    f x b = if p x
+      then x : b
+      else b
 
 myPartition :: (a -> Bool) -> [a] -> ([a], [a])
-myPartition p = notImplementedYet
-
+myPartition p xs = (myFilter p xs, myFilter (not . p) xs)
